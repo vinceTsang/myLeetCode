bc8400170b627c1f19bf60502a63a7aefd790dec
Vincent Tsang <vinceTsang@users.noreply.github.com>
2016-03-27 20:51:10 +0800

    Update 235-LCA-of-BST.cpp

--- 235-LCA-of-BST.cpp
+++ 235-LCA-of-BST.cpp
@@ -1,3 +1,14 @@
+/**
+ * 对于一个二分查找树
+ * 记p->val < q->val
+ * 若节点x是p和q的最小公共祖先
+ * 那么x恰好位于p和q开始分叉的地方
+ * 即满足 p->val  <  x->val  <  q->val
+ * 无论x再往上移必不再满足
+ * 再往下移可能仍满足
+ * 故从根往下寻找第一个满足上述条件的节点即可
+ * 考虑到一个节点可以将自己称为自己的祖先，故上式可取等号
+ */
 TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
     if(root==NULL)
         return root;
